a2002f6e349d47bfd0f87145a2fe091001531e86
carzyprogrammer <103437672+carzyprogrammer@users.noreply.github.com>
2022-05-19 09:26:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function sendsms(){
         mydiv=document.getElementById("ansbox");
         mydiv.append(welement);
         welement.style.color="yellow",fontsize="10";
-        name1=document.getElementById("user1"\n).value;
+        name1=document.getElementById("user1").value;
         mydiv.append(name1);
         document.getElementById("user1").value="";
         const chars = text.split("");
@@ -21,7 +21,7 @@ function sendsms(){
         mydiv=document.getElementById("ansbox");
         mydiv.append(welement);
         welement.style.color="blue";
-        name2=document.getElementById("user2"\n).value;
+        name2=document.getElementById("user2").value;
         mydiv.append(name2);
         document.getElementById("user2").value="";
         const chars = text.split("");
